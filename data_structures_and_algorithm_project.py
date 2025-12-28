1cb42edda9af8a206010f2c55cb1ba95b80727fe
AceFVA <acefrancisagustin@gmail.com>
2025-12-28 19:39:47 +0800

    created the initial layout of the tkinter app

--- data_structures_and_algorithm_project.py
+++ data_structures_and_algorithm_project.py
@@ -13,10 +13,25 @@
 
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Binary Tree")
-root.geometry("1920x1080")
+class BinaryTreeGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Binary Tree")
+        self.root.geometry("1920x1080")
 
+        self.main_frame = tk.Frame(self.root)
+        self.main_frame.pack(fill = tk.BOTH, expand = True, padx = 20, pady = 20)
+
+        self.canvas = tk.Canvas(self.main_frame, width = 1300, height = 500, bg = "white")
+        self.canvas.pack(side = tk.LEFT, padx = 10, pady = 20)
+
+        self.button_frame = tk.Frame(self.main_frame, width = 200)
+        self.button_frame.pack(side = tk.RIGHT, pady = 20)
+
+        self.control_buttons()
+
+root = tk.Tk()
+app = BinaryTreeGUI(root)
 root.mainloop()
 
 # Program 4: Binary Search Tree
